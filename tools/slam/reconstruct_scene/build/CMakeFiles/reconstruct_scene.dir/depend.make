8af5ca6131e36b7735efe8eea620cc6d90496b59
HanshangZhu <zcabhz4@ucl.ac.uk>
2026-03-14 13:39:15 +0000

    chore: remove build artifacts from tracking, fix stale deletion

--- tools/slam/reconstruct_scene/build/CMakeFiles/reconstruct_scene.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for reconstruct_scene.
-# This may be replaced when dependencies are built.
--- tools/slam/reconstruct_scene/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hz/COMP0225_LRC_stack/tools/reconstruct_scene
-# Build directory: /home/hz/COMP0225_LRC_stack/tools/reconstruct_scene/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
